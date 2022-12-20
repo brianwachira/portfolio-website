8c4b3b51cac2c226c846fff5588b4b8fd19693eb
brianwachira <brianwachira7@gmail.com>
2022-12-20 13:43:26 +0300

    [feat] : add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        spartanRegular: ['SPARTAN-REGULAR'],
+        spartanMedium: ['SPARTAN-MEDIUM'],
+        spartanSemiBold: ['SPARTAN-SEMIBOLD'],
+        spartanBold: ['SPARTAN-BOLD'],
+      },
+      colors: {
+        'pw-blue': '#000C24',
+        'pw-light': '#FBFBFB',
+        'pw-orange': '#FBFBFB',
+        'pw-grey': '#928A97'
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
