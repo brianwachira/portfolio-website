87fc1c81388741edc4b5cee39cf55a113f40fc9f
brianwachira <brianwachira7@gmail.com>
2022-12-23 16:17:35 +0300

    [feat] : add social media links

---
+++ constants/SocialMediaProfile.ts
@@ -0,0 +1,17 @@
+export const SocialMediaProfile = [
+    {
+        name: 'Github',
+        href: "https://github.com/brianwachira",
+        icon: "/icons/github-filled.svg"
+    },
+    {
+        name: 'LinkedIn',
+        href: "https://www.linkedin.com/in/brianwachira/",
+        icon: "/icons/linkedin-with-circle.svg"
+    },
+    {
+        name: 'Twitter',
+        href: "https://twitter.com/mr_brianwachira",
+        icon: "/icons/twitter-circle-filled.svg"
+    }
+]
