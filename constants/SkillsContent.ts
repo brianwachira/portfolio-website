163d30e2d1b672fe016b1c0f068516d27283dbc3
brianwachira <brianwachira7@gmail.com>
2022-12-25 00:03:56 +0300

    [feat] : add skills section

---
+++ constants/SkillsContent.ts
@@ -0,0 +1,113 @@
+export const SkillsContent = {
+    title: 'Skills.',
+    description: 'With a diverse range of skills, including expertise in ReactJS, React Native, NodeJS, MongoDB, and GraphQL, I am a versatile software engineer with the ability to tackle a wide variety of projects and challenges.',
+    expertise: [
+        {
+            id: 1,
+            name: 'Front-end Development',
+            skills: [
+                {
+                    id: 4,
+                    name: 'HTML',
+                    icon: '/icons/html-5.png'
+                },
+                {
+                    id: 5,
+                    name: 'CSS',
+                    icon: '/icons/css.png'
+                },
+                {
+                    id: 6,
+                    name: 'SASS',
+                    icon: '/icons/sass.png'
+                },
+                {
+                    id: 2,
+                    name: 'Javascript',
+                    icon: '/icons/js.png'
+                },
+                {
+                    id: 3,
+                    name: 'Typescript',
+                    icon: '/icons/typescript.png'
+                },
+                {
+                    id: 7,
+                    name: 'React',
+                    icon: '/icons/react-2.svg'
+                },
+                {
+                    id: 8,
+                    name: 'NextJS',
+                    icon: '/icons/next-js.svg'
+                },
+                {
+                    id: 9,
+                    name: 'Bootstrap',
+                    icon: '/icons/bootstrap.png'
+                },
+                {
+                    id: 10,
+                    name: 'TailwindCSS',
+                    icon: '/icons/tailwindcss.svg'
+                },
+
+            ]
+        },
+        {
+            id: 11,
+            name: 'Mobile App Development',
+            skills: [
+                {
+                    id: 12,
+                    name: 'React Native',
+                    icon: '/icons/react-native-1.svg'
+                },
+
+            ]
+        },
+        {
+            id: 13,
+            name: 'Backend Development',
+            skills: [
+                {
+                    id: 14,
+                    name: 'NodeJS',
+                    icon: '/icons/nodejs-1.svg',
+                },
+                {
+                    id: 15,
+                    name: 'ExpressJS',
+                    icon: '/icons/express-109.svg',
+                },
+                {
+                    id: 16,
+                    name: 'GraphQL',
+                    icon: '/icons/graphql-logo-2.svg'
+                },
+                {
+                    id: 17,
+                    name: 'MongoDB',
+                    icon: '/icons/mongodb-icon-1.svg'
+                },
+
+            ]
+        },
+        {
+            id: 18,
+            name: 'UI Design',
+            skills: [
+                {
+                    id: 19,
+                    name: 'Figma',
+                    icon: '/icons/figma-1.svg'
+                },
+                {
+                    id: 20,
+                    name: 'Adobe XD',
+                    icon: '/icons/adobe-xd-2.svg'
+                }
+            ]
+        }
+    ]
+}
