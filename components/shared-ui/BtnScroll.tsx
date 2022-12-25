450c6bcb6487045ba51738a2c607a9e9a1d2dc11
brianwachira <brianwachira7@gmail.com>
2022-12-26 01:36:17 +0300

    [feat] : add projects section

---
+++ components/shared-ui/BtnScroll.tsx
@@ -0,0 +1,17 @@
+const BtnScroll = () => {
+    return (
+        <div className="flex items-center my-8">
+            <img
+                src="/icons/arrow-down-blue-icon.svg"
+                alt="arrow-icon-down"
+                className="animate-pulse mr-2"
+            />
+            <span className=" text-xs font-spartanMedium text-pw-blue">
+                Scroll
+            </span>
+        </div>
+
+    )
+}
+
+export default BtnScroll
