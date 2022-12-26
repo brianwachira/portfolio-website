d3a665b91cdeaa73aefc67139f98b7069b17f609
brianwachira <brianwachira7@gmail.com>
2022-12-26 14:11:40 +0300

    [feat] : make btnScroll component reusable by adding light theme

--- components/shared-ui/BtnScroll.tsx
+++ components/shared-ui/BtnScroll.tsx
@@ -1,17 +1,33 @@
-const BtnScroll = () => {
-    return (
-        <div className="flex items-center my-8">
-            <img
-                src="/icons/arrow-down-blue-icon.svg"
-                alt="arrow-icon-down"
-                className="animate-pulse mr-2"
-            />
-            <span className=" text-xs font-spartanMedium text-pw-blue">
-                Scroll
-            </span>
-        </div>
-
-    )
+interface IBtnScrollProps {
+	light?: boolean;
 }
+const BtnScroll = (props: IBtnScrollProps) => {
+	const { light } = props;
 
-export default BtnScroll
+	return (
+		<>
+            {light ? 
+            <>
+            <div className="flex items-center my-8">
+                <img
+                    src="/icons/arrow-down-icon.svg"
+                    alt="arrow-icon-down"
+                    className="animate-pulse mr-2"
+                />
+                <span className=" text-xs font-spartanMedium text-pw-grey">
+                    Scroll
+                </span>
+            </div></>:<>
+			<div className="flex items-center my-8">
+				<img
+					src="/icons/arrow-down-blue-icon.svg"
+					alt="arrow-icon-down"
+					className="animate-pulse mr-2"
+				/>
+				<span className=" text-xs font-spartanMedium text-pw-blue">Scroll</span>
+			</div></>}
+		</>
+	);
+};
+
+export default BtnScroll;
