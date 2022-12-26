4ee44614b4d453bcf8241699c84f2b04dd0c2ecf
brianwachira <brianwachira7@gmail.com>
2022-12-26 14:11:03 +0300

    [feat] : add previous and next buttons and pagination

--- components/sections/SectionProjects.tsx
+++ components/sections/SectionProjects.tsx
@@ -3,6 +3,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 import { ProjectsContent } from "../../constants/ProjectsContent";
 import Link from "next/link";
@@ -16,8 +21,16 @@ const SectionProjects = () => {
 			className="container lg:mx-auto grid min-h-screen py-20 xl:px-20"
 		>
 			<div className="grid self-start" />
-			<h5 className="text-pw-orange font-spartanBold text-3xl">{title}</h5>
-			<Swiper slidesPerView={"auto"}>
+			<Swiper
+				slidesPerView={"auto"}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				navigation
+				pagination
+				modules={[Autoplay, Navigation, Pagination]}
+			>
 				{projects.map((project) => (
 					<SwiperSlide
 						key={project.id}
@@ -37,66 +50,71 @@ const SectionProjects = () => {
 								/>
 							</div>
 						</div>
-						<div className="grid self-center">
-							<h5 className=" text-pw-orange font-spartanSemiBold text-2xl mb-6">
-								{project.title}
-							</h5>
-							<p className="text-pw-blue font-spartanRegular text-2xl mb-6">
-								{project.description}
-							</p>
-							<div className="flex flex-wrap mb-6">
-								{project.techStack.map((stack) => (
-									<span
-										key={stack.id}
-										className="cursor-pointer py-2 px-3 bg-pw-grey text-sm font-spartanMedium text-white rounded mr-4 transition delay-150 duration-300 ease-in-out hover:bg-pw-orange"
+						<div className="grid">
+							<div className="grid self-center">
+								<h5 className="text-pw-orange font-spartanBold text-3xl mb-6 ">
+									{title}
+								</h5>
+								<h5 className=" text-pw-orange font-spartanSemiBold text-2xl mb-6">
+									{project.title}
+								</h5>
+								<p className="text-pw-blue font-spartanRegular text-2xl mb-6">
+									{project.description}
+								</p>
+								<div className="flex flex-wrap mb-6">
+									{project.techStack.map((stack) => (
+										<span
+											key={stack.id}
+											className="cursor-pointer py-2 px-3 bg-pw-grey text-sm font-spartanMedium text-white rounded mr-4 transition delay-150 duration-300 ease-in-out hover:bg-pw-orange"
+										>
+											{stack.stack}
+										</span>
+									))}
+								</div>
+								<div className="inline-flex">
+									<Link
+										href={project.githubLink}
+										target="_blank"
+										className={`${
+											project.githubLink.length < 1 && "hidden"
+										} "transition delay-150 duration-300 ease-in-out hover:scale-125"`}
 									>
-										{stack.stack}
-									</span>
-								))}
-							</div>
-							<div className="inline-flex">
-								<Link
-									href={project.githubLink}
-									target="_blank"
-									className={`${
-										project.githubLink.length < 1 && "hidden"
-									} "transition delay-150 duration-300 ease-in-out hover:scale-125"`}
-								>
-									<img
-										src="/icons/github-filled.svg"
-										alt="github link"
-										className="mr-4"
-									/>
-								</Link>
-								<Link
-									href={project.websiteLink}
-									target="_blank"
-									className={`${
-										project.websiteLink.length < 1 && "hidden"
-									} "transition delay-150 duration-300 ease-in-out hover:scale-125"`}
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={1.5}
-										stroke="currentColor"
-										className="text-pw-grey w-[37px] h-[37px] mr-4"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+										<img
+											src="/icons/github-filled.svg"
+											alt="github link"
+											className="mr-4"
 										/>
-									</svg>
-								</Link>
+									</Link>
+									<Link
+										href={project.websiteLink}
+										target="_blank"
+										className={`${
+											project.websiteLink.length < 1 && "hidden"
+										} "transition delay-150 duration-300 ease-in-out hover:scale-125"`}
+									>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="text-pw-grey w-[37px] h-[37px] mr-4"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+											/>
+										</svg>
+									</Link>
+								</div>
 							</div>
 						</div>
 					</SwiperSlide>
 				))}
 			</Swiper>
 			<div className="grid self-end">
-				<BtnScroll/>
+				<BtnScroll light/>
 			</div>
 		</section>
 	);
