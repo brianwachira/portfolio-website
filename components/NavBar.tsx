4a81eee9f5ac913d267009b701e8292a2514312b
brianwachira <brianwachira7@gmail.com>
2022-12-29 18:06:33 +0300

    [enhancement] : make navbar responsive on mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,21 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { Routes } from "../constants/Routes";
 import useScrollPosition from "../hooks/useScrollPosition";
 const NavBar = () => {
 	const router = useRouter();
 
 	const scrollPosition = useScrollPosition()
+
+	const [navIsOpen, setNavIsOpen] = useState(false);
+
+	const toggleNav = () => setNavIsOpen(!navIsOpen)
+
 	return (
-		<nav className={`px-2 sm:px-4 fixed w-full z-20 top-0 left-0  transition delay-150 duration-300 ease-in-out ${scrollPosition > 500 ? 'bg-pw-blue' : ''}`}>
+		<nav className={`px-2 py-2 sm:px-4 fixed w-full z-20 top-0 left-0  transition delay-150 duration-300 ease-in-out bg-pw-blue ${scrollPosition > 500 ? 'md:bg-pw-blue' : 'md:bg-transparent'}`}>
 			<div className="lg:mx-auto container flex flex-wrap items-center justify-between">
-				<div className="flex md:order-2">
+				<div className="flex md:order-2 justify-end">
 					<button
 						data-collapse-toggle="navbar-sticky"
 						type="button"
 						className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
 						aria-controls="navbar-sticky"
 						aria-expanded="false"
+						onClick={toggleNav}
 					>
 						<span className="sr-only">Open main menu</span>
 						<svg
@@ -34,26 +41,27 @@ const NavBar = () => {
 					</button>
 				</div>
 				<div
-					className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+					className={`${navIsOpen === true ? '' : 'hidden'} items-center justify-between  w-full md:flex md:w-auto md:order-1`}
 					id="navbar-sticky"
 				>
 					<ul className="lg:px-12 items-center flex flex-col mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 ">
 						{Routes.map((route) => (
-							<li key={route.href}>
+							<li key={route.href}
+							onClick={toggleNav}>
 								{router.asPath === "/".concat(route.href === '/' ? "" : route.href) ? (
 									<Link
 										href={route.href}
-										className="bg-[url('/images/linkActiveBackground.svg')] bg-cover animate-pulse w-40 h-28 bg-center text-center align-middle bg-no-repeat  z-10 grid py-2 pl-3 pr-4"
+										className="md:bg-[url('/images/linkActiveBackground.svg')] md:bg-cover md:animate-pulse md:w-40 md:h-28 md:bg-center md:text-center md:align-middle md:bg-no-repeat  z-10 grid py-2 md:pl-3 md:pr-4"
 										aria-current="page"
 									>
-										<span className="grid self-center -ml-6 text-pw-orange text-lg font-spartanMedium">
+										<span className="grid self-center md:-ml-6 text-pw-orange text-lg font-spartanMedium">
 											{route.title}
 										</span>
 									</Link>
 								) : (
 									<Link
 										href={route.href}
-										className="block py-2 pl-3 pr-4 text-pw-grey text-lg font-medium font-spartanMedium rounded md:bg-transparent md:p-0"
+										className="block py-2 md:pl-3 md:pr-4 text-pw-grey text-lg font-medium font-spartanMedium rounded md:bg-transparent md:p-0"
 										aria-current="page"
 									>
 										{route.title}
