29d52837c297100fd699bcb4469f31228d9e1710
brianwachira <brianwachira7@gmail.com>
2022-12-30 15:29:39 +0300

    [feat] : add privacy policy link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,26 +24,35 @@ const Footer = () => {
 					</div>
 				</div>
 				<div className="flex justify-center">
-				<span className=" text-white text-sm font-spartanMedium text-center">
-					Design inspiration from
-					<Link
-						href={"https://www.figma.com/community/file/1006095821656678611"}
-						target="_blank"
-						className=" text-sm font-spartanBold  text-pw-orange"
-					>
-						&nbsp;Akhil T J Portfolio Mockup Design{" "}
-					</Link>
-					<img
-						src="/icons/figma-1.svg"
-						alt=""
-						className="w-5 h-5 animate-pulse inline-flex"
-					/>
-				</span>
-
+					<span className=" text-white text-sm font-spartanMedium text-center">
+						Design inspiration from
+						<Link
+							href={"https://www.figma.com/community/file/1006095821656678611"}
+							target="_blank"
+							className=" text-sm font-spartanBold  text-pw-orange"
+						>
+							&nbsp;Akhil T J Portfolio Mockup Design{" "}
+						</Link>
+						<img
+							src="/icons/figma-1.svg"
+							alt=""
+							className="w-5 h-5 animate-pulse inline-flex"
+						/>
+					</span>
 				</div>
-					<h5 className=" text-white text-sm font-spartanMedium text-center mt-5">
-						All Rights Reserved © {year}
-					</h5>
+				<div className="flex items-center justify-center mt-5">
+				<h5 className=" text-white text-sm font-spartanMedium text-center">
+					All Rights Reserved © {year}
+				</h5>
+				<Link
+					href={"/privacypolicy"}
+					target="_blank"
+					className=" inline-flex text-xs font-spartanBold  text-pw-orange ml-5"
+				>
+					Privacy Policy
+				</Link>
+					
+				</div>
 			</main>
 		</footer>
 	);
