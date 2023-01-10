a69e8ea7acfaea975acd7dea6ce93923842ee430
brianwachira <brianwachira7@gmail.com>
2023-01-10 15:51:33 +0300

    [refactor] : indentation

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,14 +6,18 @@ import useScrollPosition from "../hooks/useScrollPosition";
 const NavBar = () => {
 	const router = useRouter();
 
-	const scrollPosition = useScrollPosition()
+	const scrollPosition = useScrollPosition();
 
 	const [navIsOpen, setNavIsOpen] = useState(false);
 
-	const toggleNav = () => setNavIsOpen(!navIsOpen)
+	const toggleNav = () => setNavIsOpen(!navIsOpen);
 
 	return (
-		<nav className={`px-2 py-2 sm:px-4 fixed w-full z-20 top-0 left-0  transition delay-150 duration-300 ease-in-out bg-pw-blue ${scrollPosition > 500 ? 'md:bg-pw-blue' : 'md:bg-transparent'}`}>
+		<nav
+			className={`px-2 py-2 sm:px-4 fixed w-full z-20 top-0 left-0  transition delay-150 duration-300 ease-in-out bg-pw-blue ${
+				scrollPosition > 500 ? "md:bg-pw-blue" : "md:bg-transparent"
+			}`}
+		>
 			<div className="lg:mx-auto container flex flex-wrap items-center justify-between">
 				<div className="flex md:order-2 justify-end">
 					<button
@@ -41,14 +45,16 @@ const NavBar = () => {
 					</button>
 				</div>
 				<div
-					className={`${navIsOpen === true ? '' : 'hidden'} items-center justify-between  w-full md:flex md:w-auto md:order-1`}
+					className={`${
+						navIsOpen === true ? "" : "hidden"
+					} items-center justify-between  w-full md:flex md:w-auto md:order-1`}
 					id="navbar-sticky"
 				>
 					<ul className="lg:px-12 items-center flex flex-col mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 ">
 						{Routes.map((route) => (
-							<li key={route.href}
-							onClick={toggleNav}>
-								{router.asPath === "/".concat(route.href === '/' ? "" : route.href) ? (
+							<li key={route.href} onClick={toggleNav}>
+								{router.asPath ===
+								"/".concat(route.href === "/" ? "" : route.href) ? (
 									<Link
 										href={route.href}
 										className="md:bg-[url('/images/linkActiveBackground.svg')] md:bg-cover md:animate-pulse md:w-40 md:h-28 md:bg-center md:text-center md:align-middle md:bg-no-repeat  z-10 grid py-2 md:pl-3 md:pr-4"
