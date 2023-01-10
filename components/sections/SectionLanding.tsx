0cda6c8c0ce245888a074623e2f20030f78aa802
brianwachira <brianwachira7@gmail.com>
2023-01-10 13:19:08 +0300

    [refactor] : use content from landingPageContent constant and socialmediabutton component

--- components/sections/SectionLanding.tsx
+++ components/sections/SectionLanding.tsx
@@ -1,28 +1,23 @@
-import Link from "next/link";
-import { SocialMediaProfile } from "../../constants/SocialMediaProfile";
+import { socialMediaProfileContent } from "../../constants";
 import BtnScroll from "../shared-ui/BtnScroll";
+import { landingPageContent } from "../../constants";
+import SocialMediaButton from "../SocialMediaButton";
 
 const SectionLanding = () => {
+	const { title, description } = landingPageContent;
 	return (
 		<div className="container md:mx-auto grid h-[calc(100vh)] mt-28">
 			<div className="grid self-start"></div>
 			<div className="mx-auto grid-self-center mt-36 lg:px-20 container">
 				<h1 className=" font-spartanExtraBold text-4xl stroke-text text-center lg:text-left">
-					Brian Wachira
+					{title}
 				</h1>
 				<h2 className="font-spartanSemiBold text-2xl text-white my-4 text-center lg:text-left">
-					Software Engineer
+					{description}
 				</h2>
 				<div className="flex justify-center lg:justify-start">
-					{SocialMediaProfile.map((profile) => (
-						<Link
-							key={profile.name}
-							href={profile.href}
-							target="_blank"
-							className="transition delay-150 duration-300 ease-in-out hover:scale-125"
-						>
-							<img src={profile.icon} alt={profile.name} className=" mr-4"   loading="lazy"/>
-						</Link>
+					{socialMediaProfileContent.map((item) => (
+						<SocialMediaButton key={item.name} item={item} />
 					))}
 				</div>
 			</div>
