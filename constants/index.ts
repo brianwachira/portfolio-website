f17b9843dea49715fc4d57410c7248a252cc21a6
brianwachira <brianwachira7@gmail.com>
2024-01-06 14:31:51 +0300

    Update index.ts
    - Update projects

--- constants/index.ts
+++ constants/index.ts
@@ -80,6 +80,28 @@ export const projectsContent = {
     projects: [
         {
             id: 1,
+            title: 'droidconKE2023ReactNative',
+            description: 'React Native App for the 4th Android Developer Conference in Kenya - droidcon to be held in Nairobi from November 8th-10th 2023.',
+            techStack: [
+                {
+                    id: 2,
+                    stack: 'Expo ( React Native )'
+                },
+                {
+                    id: 3,
+                    stack: 'React Query'
+                },
+                {
+                    id: 4,
+                    stack: 'React Testing Library'
+                }
+            ],
+            websiteLink: 'https://play.google.com/store/apps/details?id=com.ke.droidcon.reactnative&hl=en&gl=US',
+            githubLink: 'https://github.com/droidconKE/droidconKE2023ReactNative',
+            image: 'images/droidconkereactnative.jpeg'
+        },
+        {
+            id: 2,
             title: 'droidconKE2022ReactNative',
             description: 'React Native App for the 3rd Android Developer Conference in Kenya - droidcon to be held in Nairobi from November 16-18th 2022.',
             techStack: [
@@ -99,7 +121,68 @@ export const projectsContent = {
             websiteLink: 'https://play.google.com/store/apps/details?id=com.brianwachira.droidconKE2022ReactNative&hl=en&gl=US',
             githubLink: 'https://github.com/droidconKE/droidconKE2022ReactNative',
             image: 'images/droidconkereactnative.jpeg'
+        }, 
+        {
+            id: 3,
+            title: 'Sales Reps App',
+            description: 'An app for managing sales reps in pharmaceutical companies',
+            techStack: [
+                {
+                    id: 2,
+                    stack: 'Expo ( React Native )'
+                },
+                {
+                    id: 3,
+                    stack: 'React Query'
+                },
+                {
+                    id: 4,
+                    stack: 'React Testing Library'
+                }
+            ],
+            websiteLink: '#',
+            githubLink: '#',
+            image: 'images/droidconkereactnative.jpeg'
         },
+        {
+            id: 4,
+            title: 'Restuarants App',
+            description: 'An app for ordering food from restuarants and managing the restuarants',
+            techStack: [
+                {
+                    id: 1,
+                    stack: 'React Native'
+                },
+                {
+                    id: 2,
+                    stack: 'React Testing Library'
+                }
+            ],
+            websiteLink: '#',
+            githubLink: '#',
+            image: 'images/droidconkereactnative.jpeg'
+        },{
+            id: 5,
+            title: 'Church App',
+            description: 'Coming up with IOS design guidelines for an existing android mobile application using Figma. Translating the design into a mobile app using Expo (React Native) and Typescript. Writing tests for the components used using testing library ( React Native ). Error tracking and monitoring using Sentry',
+            techStack: [
+                {
+                    id: 1,
+                    stack: 'React Native'
+                },
+                {
+                    id: 2,
+                    stack: 'Figma'
+                },
+                {
+                    id: 3,
+                    stack: 'Typescript'
+                }
+            ],
+            websiteLink: '#',
+            githubLink: '#',
+            image: 'images/droidconkereactnative.jpeg'
+        }
     ]
 }
 
